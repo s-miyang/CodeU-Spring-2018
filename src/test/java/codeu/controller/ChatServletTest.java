d9e3adbb72839dd81a093465cddb43e570e3054b
nyc-1 <36973582+nyc-1@users.noreply.github.com>
2018-04-05 20:16:17 -0700

    Beginning of emoji test

--- src/test/java/codeu/controller/ChatServletTest.java
+++ src/test/java/codeu/controller/ChatServletTest.java
@@ -196,4 +196,25 @@ public class ChatServletTest {
 
     Mockito.verify(mockResponse).sendRedirect("/chat/test_conversation");
   }
+  
+  @Test
+  public void testDoPost_Emoji() throws IOException, ServletException {
+    Mockito.when(mockRequest.getRequestURI()).thenReturn("/chat/test_conversation");
+    Mockito.when(mockSession.getAttribute("user")).thenReturn("test_username");
+
+    User fakeUser = new User(UUID.randomUUID(), "test_username", "password", Instant.now());
+    Mockito.when(mockUserStore.getUser("test_username")).thenReturn(fakeUser);
+
+    Conversation fakeConversation =
+        new Conversation(UUID.randomUUID(), UUID.randomUUID(), "test_conversation", Instant.now());
+    Mockito.when(mockConversationStore.getConversationWithTitle("test_conversation"))
+        .thenReturn(fakeConversation);
+
+    Mockito.when(mockRequest.getParameter("message"))
+        .thenReturn("Contains <b>html</b> and <script>JavaScript</script> content.");
+
+    chatServlet.doPost(mockRequest, mockResponse);
+    
+    // Not sure what to do past here
+  }
 }
