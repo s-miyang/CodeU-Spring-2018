bd22c0e56f8860387859658111275a495cecd114
Emily Zhu <zhu.emily@gmail.com>
2018-04-09 13:56:04 -0700

    implement logout

--- src/main/java/codeu/controller/LogoutServlet.java
+++ src/main/java/codeu/controller/LogoutServlet.java
@@ -58,7 +58,7 @@ public class LogoutServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    HttpServletRequest.logout();
-    request.getRequestDispatcher("/index.jsp").forward(request,response);
+    HttpSession session=request.getSession();
+    session.invalidate();
   }
 }
