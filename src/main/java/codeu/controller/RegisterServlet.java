99cf56f8aca38fe763e943b9102065799f07cde2
Emily Zhu <zhu.emily@gmail.com>
2018-04-08 21:55:12 -0700

    profile servlet added

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -72,7 +72,7 @@ public class RegisterServlet extends HttpServlet {
 	  User user = new User(UUID.randomUUID(), username, passwordHash, Instant.now());
 	  userStore.addUser(user);
 	  response.sendRedirect("/login");
-		response.sendRedirect("/users/" + user.name);
+		response.sendRedirect("/users/" + user.getName());
 	  //response.getWriter().println("<p>Username : " + username + "</p>");
 	  //response.getWriter().println("<p>Password : " + password + "</p>");
   }
