2f3eb995e2d5fcf2cc15db8405fdca7d4f828132
Emily Zhu <zhu.emily@gmail.com>
2018-04-09 19:52:12 -0700

    trying logout again

--- src/main/java/codeu/controller/LogoutServlet.java
+++ src/main/java/codeu/controller/LogoutServlet.java
@@ -58,7 +58,13 @@ public class LogoutServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    request.getSession().invalidate();
     request.getRequestDispatcher("/WEB-INF/view/logout.jsp").forward(request, response);
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    request.getSession().invalidate();
+    response.sendRedirect("/index.jsp");
   }
 }
