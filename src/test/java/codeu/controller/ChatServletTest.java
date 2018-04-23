d8b4685962ef0387fed758339c16bb8a5d298634
hmc-shark-s18 <apunt@cs.hmc.edugit config --global -lgit config --global user.name CS70>
2018-04-23 00:08:30 -0700

    new emoji test

--- src/test/java/codeu/controller/ChatServletTest.java
+++ src/test/java/codeu/controller/ChatServletTest.java
@@ -212,15 +212,15 @@ public class ChatServletTest {
     Mockito.when(mockConversationStore.getConversationWithTitle("test_conversation"))
         .thenReturn(fakeConversation);
 
-    Mockito.when(mockRequest.getParameter("message"))
-        .thenReturn(":smiley:");
+    Mockito.when(mockRequest.getParameter("message")).thenReturn(":japanese_goblin:");
 
     chatServlet.doPost(mockRequest, mockResponse);
-    
+
     ArgumentCaptor<Message> messageArgumentCaptor = ArgumentCaptor.forClass(Message.class);
     Mockito.verify(mockMessageStore).addMessage(messageArgumentCaptor.capture());
-    Assert.assertEquals("&#x1F603;", messageArgumentCaptor.getValue().getContent());
+    Assert.assertEquals("&#x1f47a;", messageArgumentCaptor.getValue().getContent());
 
     Mockito.verify(mockResponse).sendRedirect("/chat/test_conversation");
   }
+
 }
